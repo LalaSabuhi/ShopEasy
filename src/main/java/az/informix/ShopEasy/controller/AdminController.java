94cf19fc31b662d96092c6d12cf1fd081d96b604
Lale Mammadli <yavuzadpu@gmail.com>
2024-08-07 14:17:29 +0400

    added CommonService

--- src/main/java/az/informix/ShopEasy/controller/AdminController.java
+++ src/main/java/az/informix/ShopEasy/controller/AdminController.java
@@ -5,10 +5,18 @@ import az.informix.ShopEasy.service.CategoryService;
 import jakarta.servlet.http.HttpSession;
 import jdk.jfr.Registered;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @Controller
 @RequestMapping("/admin")
@@ -29,7 +37,8 @@ public class AdminController {
     }
 
     @PostMapping("/saveCategory")
-    public String saveCategory(@ModelAttribute Category category, @RequestParam("file") MultipartFile file, HttpSession session){
+    public String saveCategory(@ModelAttribute Category category, @RequestParam("file") MultipartFile file, HttpSession session)
+    throws IOException {
         String imageName = file != null ? file.getOriginalFilename() : "default.jpg";
         category.setImageName(imageName);
         Boolean existCategory =  categoryService.existCategory(category.getName());
@@ -40,6 +49,10 @@ public class AdminController {
            if(ObjectUtils.isEmpty(saveCategory)){
                session.setAttribute("errorMsg", "Not saved : internal server error");
            }else{
+               File saveFile = new ClassPathResource("static/img").getFile();
+               Path path = Paths.get(saveFile.getAbsolutePath()+File.separator + "category_img"+File.separator+ file.getOriginalFilename());
+               System.out.println(path);
+               Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
                session.setAttribute("successMsg","Saved successfully");
            }
        }
--- src/main/java/az/informix/ShopEasy/service/CategoryService.java
+++ src/main/java/az/informix/ShopEasy/service/CategoryService.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-
+@Service
 public interface CategoryService {
     public Category saveCategory(Category category);
     public List<Category> getAllCategory();
